f88d907761019d6310aa4deb0bbdde4858133eec
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-06-10 18:50:07 +0500

    first commit

---
+++ src/test/java/TestRunner/Run.java
@@ -0,0 +1,20 @@
+package TestRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        //features=".//Feature/Customers.feature/",
+        features = {".//Feature/Customers.feature/"},
+        glue = "StepDefinition",
+        tags = "@smoke",
+        dryRun = true,
+        monochrome = true,
+        plugin = {"pretty", "json:target/cucumer-reports/report.json", "html:target/cucumer-reports/report.html"})
+//plugin={"pretty","html:target/cucumer-reports/report.html"}
+public class Run {
+    /*This class is empty*/
+}
